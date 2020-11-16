acf1d1f64a0ebcf8e1b6d77a60f3a8b6c1d28cd2
SardarBadarSaghir <dataentrybadar@gmail.com>
2020-11-17 00:26:53 +0500

    Authentication Implementation

--- app/src/main/java/com/developonetwork/blumo/MainActivity.java
+++ app/src/main/java/com/developonetwork/blumo/MainActivity.java
@@ -1,14 +1,96 @@
 package com.developonetwork.blumo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mPhoneNumber, mCode;
+    private Button mSend;
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseApp.initializeApp(this);
+
+        mPhoneNumber = findViewById(R.id.phoneNumber);
+        mCode = findViewById(R.id.verificationCode);
+        mSend = findViewById(R.id.send);
+
+        //This will be waiting for button
+        mSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //This Function verification
+                startPhoneNumberVerification();
+            }
+        });
+        //After Completion iF phone is verified, Than callback chk
+        mCallBack = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                signInWithPhoneCredntials(phoneAuthCredential);
+
+            }
+
+            @Override
+            public void onVerificationFailed(@NonNull FirebaseException e) {
+            }
+        };
+    }
+
+    private void signInWithPhoneCredntials(PhoneAuthCredential phoneAuthCredential) {
+
+        FirebaseAuth.getInstance().signInWithCredential(phoneAuthCredential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) UserIsLoggedIn();
+            }
+        });
+
+    }
+
+    private void UserIsLoggedIn() {
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            startActivity(new Intent(getApplicationContext(), MainPageActivity.class));
+        }
+    }
+
+    private void startPhoneNumberVerification() {
+        //We will Send Auth Data To Firebase
+        PhoneAuthOptions options =
+                PhoneAuthOptions
+                        .newBuilder(FirebaseAuth.getInstance())
+                        .setPhoneNumber(String.valueOf(mPhoneNumber))       // Phone number to verify
+                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                        .setActivity(this)                 // Activity (for callback binding)
+                        .setCallbacks(mCallBack)          // OnVerificationStateChangedCallbacks
+                        .build();
+        //send Phone verification
+        PhoneAuthProvider.verifyPhoneNumber(options);
     }
 }
